fbd2c391eec56033db92735c847430dc5013e294
stecala <ste.cala123@gmail.com>
2022-06-21 18:27:56 +0200

    fine bonus

--- scipt.js
+++ scipt.js
@@ -1,7 +1,6 @@
 const shoppingList = ['salad', 'tomato', 'soy milk', 'bread', 'pasta', 'ice cream'];
 const cartElement = document.getElementById('cart');
 let i = 0;
-let check = true;
 const arrayClick = [];
 while (i < shoppingList.length) {
     const elementToShop = document.createElement('li');
@@ -16,18 +15,15 @@ const liClick = cartElement.children;
 
 for (let i = 0; i < liClick.length; i++) {
     liClick[i].addEventListener('click', function () {
-  
-
-        if (check === false) {
+        if (arrayClick[i].classList.contains('list-group-item-warning')==true) {
+            arrayClick[i].classList.remove('list-group-item-warning');
+            arrayClick[i].classList.add('text-decoration-line-through', 'list-group-item-info');
+        
+        }
+        else {
             arrayClick[i].classList.remove('text-decoration-line-through', 'list-group-item-info');
             arrayClick[i].classList.add('list-group-item-warning');
             console.log(check);
-            check = true;
-        }
-        else {
-            arrayClick[i].classList.remove('list-group-item-warning');
-            arrayClick[i].classList.add('text-decoration-line-through', 'list-group-item-info');
-            check = false;
         }
     })
 }
