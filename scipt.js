3e38f6df17014b3c26586dce4ff2149f81c769ee
stecala <ste.cala123@gmail.com>
2022-06-21 13:55:24 +0200

    migliorie grafiche e controllo funzionamento

--- scipt.js
+++ scipt.js
@@ -1,8 +1,9 @@
-const shoppingList=['salad', 'tomato', 'soy milk', 'bread', 'pasta'];
+const shoppingList=['salad', 'tomato', 'soy milk', 'bread', 'pasta', 'ice cream'];
 const cartElement = document.getElementById('cart');
 let i = 0;
 while(i<shoppingList.length){
     const elementToShop = document.createElement('li');
+    elementToShop.classList.add('list-group-item', 'list-group-item-warning');
     elementToShop.innerText= shoppingList[i];
     cartElement.append(elementToShop);
     i++;
